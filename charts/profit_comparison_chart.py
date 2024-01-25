43d53289bfc9e4587107e4f634ff78f94aceb65c
Eric Matthes <ehmatthes@gmail.com>
2024-01-25 12:27:37 -0900

    Profit comparison chart works.

--- charts/profit_comparison_chart.py
+++ charts/profit_comparison_chart.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import matplotlib as mpl
 import pandas as pd
+import numpy as np
 
 
 def get_plot(config, df):
@@ -13,9 +14,12 @@ def get_plot(config, df):
     ss_profits = df["profits_ss"]
     gp_profits = df["profits_gp"]
     x_values = df["user_levels"]
-    df["diffs"] = pd.Series(
+    diffs = pd.Series(
         [p_gp - p_ss for p_gp, p_ss in zip(df["profits_gp"], df["profits_ss"])]
     )
+    pos_diffs = diffs.where(diffs > 0, other=np.nan)
+    neg_diffs = diffs.where(diffs < 0, other=np.nan)
+    zero_diffs = diffs.where(diffs == 0, other=np.nan)
 
     plt.style.use(["seaborn-v0_8-whitegrid", "charts/nlc_style.mplstyle"])
     fig, ax = plt.subplots()
@@ -28,7 +32,18 @@ def get_plot(config, df):
     # Define horizontal placement of all line labels.
     label_pos_x = x_values.iloc[-1] + 0.01 * config.max_subs
 
-    ax.plot(x_values, df["diffs"], color="lightblue")
+
+    # df.cum_profit.where(df.cum_profit.ge(0), np.nan).plot(color='green')
+    # df.cum_profit.where(df.cum_profit.lt(0), np.nan).plot(color='red')
+
+    # df["better"] = df["diffs"].where()
+
+
+    ax.plot(x_values, pos_diffs, color="lightgreen")
+    ax.fill_between(x_values, pos_diffs, where=pos_diffs > 0, interpolate=True, color="lightgreen", alpha=0.3)
+    ax.plot(x_values, neg_diffs, color="salmon")
+    ax.fill_between(x_values, neg_diffs, where=neg_diffs < 0, interpolate=True, color="salmon", alpha=0.3)
+    ax.plot(x_values, zero_diffs, color="black")
 
     # # Add Substack data.
     # if config.show_ss:
