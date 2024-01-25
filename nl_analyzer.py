b3e1c774f7706db3e8787e1ae1273c923f7ce392
Eric Matthes <ehmatthes@gmail.com>
2024-01-25 05:54:18 -0900

    Percent of revenue chart moved to separate module.

--- nl_analyzer.py
+++ nl_analyzer.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from pricer import Pricer
 from config import Config
 
-from charts import cost_plot
+from charts import cost_plot, por_chart
 
 
 # Build sidebar.
@@ -65,48 +65,6 @@ st.pyplot(cost_fig)
 
 "---"
 
-# --- percentage of revenue chart ---
-
-x_values = range(0, config.max_subs, 10)
-
-# # Define horizontal placement of all line labels.
-label_pos_x = x_values[-1] + 0.01 * config.max_subs
-
-fig, ax = plt.subplots()
-
-nonzero_revenue = bool(sum(pricer.df["revenues"]))
-
-if config.show_ss and nonzero_revenue:
-    ss_percentages = pricer.df["percent_rev_ss"]
-    ax.plot(x_values, ss_percentages)
-    label_pos_y = ss_percentages.iloc[-1] - 0.02 * ax.get_ylim()[1]
-    ax.annotate("Substack", (label_pos_x, label_pos_y))
-
-if config.show_gp and nonzero_revenue:
-    gp_percentages = pricer.df["percent_rev_gp"]
-    ax.plot(x_values, gp_percentages)
-    label_pos_y = gp_percentages.iloc[-1] - 0.0002 * ax.get_ylim()[1]
-    ax.annotate("Ghost Pro", (label_pos_x, label_pos_y))
-
-# Limit of y-axis needs to be at least 15%, but shouldn't over-emphasize high values
-# for only the lowest subscriber levels. Use percentage 1/10 of the way through the set
-# of values, so most of each platform's line is visible.
-if nonzero_revenue:
-    try:
-        y_max = max(0.15, gp_percentages[int(0.1 * len(gp_percentages))])
-    except NameError:
-        # Temp fix for when Ghost Pro is not selected.
-        y_max = 0.2
-    ax.axis([0, 1.05 * config.max_subs, 0, y_max])
-    y_vals = ax.get_yticks()
-    ax.set_yticklabels(["{:,.1%}".format(y_val) for y_val in y_vals])
-else:
-    x_pos = ax.get_xlim()[1] * 0.1
-    y_pos = ax.get_ylim()[1] / 2
-    ax.annotate("No revenue generated.", (x_pos, y_pos), fontsize=16)
-
-ax.set_title("Annual cost as percent of revenue")
-ax.set_xlabel("Number of subscribers")
-ax.set_ylabel("Percent of revenue")
-
-fig
+# Percent of revenue chart.
+por_fig = por_chart.get_chart(config, pricer.df)
+por_fig
