a550142d3871c52b17969c9fb105fb5a01d92679
Eric Matthes <ehmatthes@gmail.com>
2024-01-26 11:00:18 -0900

    Buttondown cost series is filled.

--- pricer.py
+++ pricer.py
@@ -162,17 +162,72 @@ class Pricer:
                 costs.append(cost)
         self.df["costs_bd"] = pd.Series(costs)
 
+    # @staticmethod
+    # def _get_cost_bd_high(num_users):
+    #     """Get the Buttondown cost for over 20k users."""
+    #     # Round up to next highest thousand.
+    #     pricing_users = ((num_users // 1_000) + 1) * 1_000
+    #     if num_users <= 40_000:
+    #         additional_cost = (pricing_users / 1_000) * 50
+    #     else:
+    #         return np.nan
+
+    #     return 1390 + additional_cost
+
     @staticmethod
     def _get_cost_bd_high(num_users):
-        """Get the Buttondown cost for over 20k users."""
-        # Round up to next highest thousand.
-        pricing_users = ((num_users // 1_000) + 1) * 1_000
-        if num_users <= 40_000:
-            additional_cost = (pricing_users / 1_000) * 50
-        else:
-            return np.nan
+        # Go through batches of subscribers, until none left.
+        thousands = int(round(num_users, -3) / 1000)
 
-        return 1390 + additional_cost
+        # --- Base tier of 20k users, $139/month
+        # Subtract 20k users, and add base cost.
+        thousands -= 20
+        monthly_cost = 139
+
+        # --- Tier 1: 20k-40k users, $5/month per 1,000 users.
+        # Subtract 20k, and look at whether that's positive or negative.
+        # If negative, find the batch size for this tier.
+        thousands -= 20
+        if thousands <= 0:
+            # Find out the batch of users in this tier.
+            batch = 20 + thousands
+            monthly_cost += 5*batch
+            return monthly_cost * 10
+
+        # This tier is full; add full tier cost
+        monthly_cost += 5*20
+
+        # --- Tier 2: 40k-60k users, $4/month per 1,000 users.
+        thousands -= 20
+        if thousands <= 0:
+            batch = 20 + thousands
+            monthly_cost += 4*batch
+            return monthly_cost * 10
+
+        # This tier is full; add full tier cost
+        monthly_cost += 4*20
+
+        # --- Tier 3: 60k-80k users, $3/month per 1,000 users.
+        thousands -= 20
+        if thousands <= 0:
+            batch = 20 + thousands
+            monthly_cost += 3*batch
+            return monthly_cost * 10
+
+        # This tier is full; add full tier cost
+        monthly_cost += 3*20
+
+        # --- Tier 4: 80k-100k users, $2/month per 1,000 users.
+        thousands -= 20
+        if thousands <= 0:
+            batch = 20 + thousands
+            monthly_cost += 2*batch
+            return monthly_cost * 10
+
+        # This tier is full; add full tier cost
+        monthly_cost += 2*20
+
+        return monthly_cost
 
 
 # Simple profiling tool.
