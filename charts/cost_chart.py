a780d3217cc601eaf0327065ca5d98d83210df4c
Eric Matthes <ehmatthes@gmail.com>
2024-01-31 07:25:34 -0900

    Remove unused code.

--- charts/cost_chart.py
+++ charts/cost_chart.py
@@ -6,43 +6,6 @@ def get_plot(nl_config, df):
     title = "Annual cost of hosting a newsletter"
     labels = {"x": "Number of subscribers", "y": "Annual cost"}
 
-    # # Define each trace. Include names for hover data.
-    # trace_gp = go.Scatter(
-    #     x=df["user_levels"],
-    #     y=df["costs_gp"],
-    #     mode="lines",
-    #     name="Ghost Pro",
-    #     line=dict(color=nl_config.gp_color),
-    # )
-    # trace_bd = go.Scatter(
-    #     x=df["user_levels"],
-    #     y=df["costs_bd"],
-    #     mode="lines",
-    #     name="Buttondown",
-    #     line=dict(color=nl_config.bd_color),
-    # )
-    # trace_bh = go.Scatter(
-    #     x=df["user_levels"],
-    #     y=df["costs_bh"],
-    #     mode="lines",
-    #     name="beehiiv",
-    #     line=dict(color=nl_config.bh_color),
-    # )
-    # trace_ss = go.Scatter(
-    #     x=df["user_levels"],
-    #     y=df["costs_ss"],
-    #     mode="lines",
-    #     name="Substack",
-    #     line=dict(color=nl_config.ss_color),
-    # )
-    # trace_ck = go.Scatter(
-    #     x=df["user_levels"],
-    #     y=df["costs_ck"],
-    #     mode="lines",
-    #     name="ConvertKit",
-    #     line=dict(color=nl_config.ck_color),
-    # )
-
     fig = go.Figure()
     for pf, name in [("gp", "Gost Pro"), ("bd", "Buttondown"), ("bh", "beehiiv"), ("ss", "Substack"), ("ck", "ConvertKit")]:
         if not getattr(nl_config, f"show_{pf}"):
@@ -57,22 +20,6 @@ def get_plot(nl_config, df):
                 line=dict(color=color),
             )
         )
-
-
-
-
-    # Create the figure and add the traces
-    # fig = go.Figure()
-    # if nl_config.show_gp:
-    #     fig.add_trace(trace_gp)
-    # if nl_config.show_bd:
-    #     fig.add_trace(trace_bd)
-    # if nl_config.show_bh:
-    #     fig.add_trace(trace_bh)
-    # if nl_config.show_ss:
-    #     fig.add_trace(trace_ss)
-    # if nl_config.show_ck:
-    #     fig.add_trace(trace_ck)
 
     # Label lines.
     for col, name, color, show in [
