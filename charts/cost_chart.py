5dbefe7566ca1e4f5b2391983193f5c01ffacd81
Eric Matthes <ehmatthes@gmail.com>
2024-01-31 07:36:48 -0900

    Lines are labeled in a more concise loop.

--- charts/cost_chart.py
+++ charts/cost_chart.py
@@ -2,15 +2,16 @@ import plotly.graph_objects as go
 
 
 def get_plot(nl_config, df):
-    """Generate the cost chart, using Plotly."""
+    """Generate the cost chart."""
     title = "Annual cost of hosting a newsletter"
     labels = {"x": "Number of subscribers", "y": "Annual cost"}
 
     platform_codes = [("gp", "Gost Pro"), ("bd", "Buttondown"), ("bh", "beehiiv"), ("ss", "Substack"), ("ck", "ConvertKit")]
 
     fig = go.Figure()
+
+    # Add trace for each visible platform.
     for pf, name in platform_codes:
-        # Don't create trace if we're not showing that platform.
         if not getattr(nl_config, f"show_{pf}"):
             continue
 
@@ -26,31 +27,19 @@ def get_plot(nl_config, df):
         )
 
     # Label lines.
-    for col, name, color, show in [
-        ("costs_gp", "Ghost Pro", nl_config.gp_color, nl_config.show_gp),
-        ("costs_bd", "Buttondown", nl_config.bd_color, nl_config.show_bd),
-        ("costs_bh", "beehiiv", nl_config.bh_color, nl_config.show_bh),
-        ("costs_ss", "Substack", nl_config.ss_color, nl_config.show_ss),
-        ("costs_ck", "ConvertKit", nl_config.ck_color, nl_config.show_ck),
-    ]:
-        if show:
-            fig.add_annotation(
-                x=df["user_levels"].iloc[-1],
-                y=df[col].iloc[-1],
-                text=name,
-                showarrow=False,
-                xanchor="left",
-                xshift=5,
-                font=dict(color=color),
-            )
-
-
-    # for pf, name in [
-    #     ("gp")
-    # ]
-
-
-
+    for code, name in platform_codes:
+        if not getattr(nl_config, f"show_{code}"):
+            continue
+        color = getattr(nl_config, f"{code}_color")
+        fig.add_annotation(
+            x=df["user_levels"].iloc[-1],
+            y=df[f"costs_{code}"].iloc[-1],
+            text=name,
+            showarrow=False,
+            xanchor="left",
+            xshift=5,
+            font=dict(color=color),
+        )
 
     # Update layout with title and axis labels
     fig.update_layout(
